83ea1ff5ebf2d2ef6a98fa4800c7a9a9934991ca
HuangYe <huangye397044815@126.com>
2018-05-28 07:35:36 +1000

    Initial commit

---
+++ BubbleFighter/BubbleFighter/ScenesControllers/MainScene.swift
@@ -0,0 +1,14 @@
+//
+//  MainScene.swift
+//  BubbleFighter
+//
+//  Created by HuangYe on 2018/5/28.
+//  Copyright © 2018年 HuangYe. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class MainScene: SKScene {
+
+}
